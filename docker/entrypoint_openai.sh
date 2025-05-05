6b48297f2519513937b0670e744852bae6c32638
kmazrolina <karolina.m.zrobek@gmail.com>
2025-05-05 22:56:24 +0200

    Add version for openAI models

---
+++ docker/entrypoint_openai.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Build llm reasoner ros package
+cd /root/catkin_ws/src
+. /opt/ros/noetic/setup.bash; cd /root/catkin_ws; catkin build
+
+
+# Set the configuration file
+source /root/catkin_ws/devel/setup.bash
+rosparam load /root/config/${CONFIG} /pose_estimator;
+
+# Run LLM agent service
+cd /root/goal_state_reasoning
+python main.py
